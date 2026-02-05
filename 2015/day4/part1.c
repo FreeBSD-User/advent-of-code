8bdbb1682fed46c42d87079910e3c9dadbc9eae1
what <>
2026-02-05 21:14:28 +0000

    2015/day4
    FossilOrigin-Name: b919b4dbd19c358357336fd9fac7069581159853024614c2d63cf9cab3bf07a9

---
+++ 2015/day4/part1.c
@@ -0,0 +1,29 @@
+#include <md5.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "advc_utils.h"
+#include "def.h"
+
+void
+main_part1()
+{
+	MD5_CTX ctx;
+	char buf[BUFSIZ], digest[33];
+	int len;
+
+	sgets(buf);
+	len = strlen(buf);
+
+	uint32_t i;
+	for (i = 0; i < UINT32_MAX; i++) {
+		snprintf(buf+len, BUFSIZ-len, "%d", i);
+		MD5Init(&ctx);
+		MD5Update(&ctx, buf, strlen(buf));
+		MD5End(&ctx, digest);
+		if (strncmp(digest, "00000", 5) == 0)
+			break;
+	}
+	printf("%d\n", i);
+}
