a4ef2c39b5b9b48e200a248d35d5d029c34b3a3b
what <>
2026-02-05 19:51:36 +0000

    day2
    FossilOrigin-Name: 24f160ddbe13d7fc52c2c76cf932eba24f56cba5da675714ed5d56491083f2c2

---
+++ 2015/day2/part1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "advc_utils.h"
+#include "def.h"
+
+void
+main_part1()
+{
+	char buf[BUFSIZ];
+	int t[3];
+	int acc = 0;
+
+	while (sgets(buf)) {
+		sscanf(buf, "%dx%dx%d", &t[0], &t[1], &t[2]);
+		int min = 1000;
+		for (int i = 0; i < 3; i++) {
+			int r = t[i] * t[(i+1)%3];
+			min = (r < min) ? r : min;
+			acc += 2*r;
+		}
+		acc += min;
+	}
+	printf("%d\n", acc);
+}
--- include/def.h
+++ include/def.h
@@ -1,7 +1,15 @@
 #ifndef DEF_H
 #define DEF_H
 
+typedef struct coord coord;
+
 void main_part1(void);
 void main_part2(void);
+
+struct coord
+{
+	int x;
+	int y;
+};
 
 #endif
